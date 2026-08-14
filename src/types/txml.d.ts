4232671c0066453d051e68774a4ecc43cc9c7579
khuonghung <khuonghung1423@gmail.com>
2026-08-14 22:50:36 +0700

    chore: enable browser and XML input types

---
+++ src/types/txml.d.ts
@@ -0,0 +1,18 @@
+declare module 'txml' {
+  export interface TxmlNode {
+    tagName: string;
+    attributes: Record<string, string>;
+    children: Array<TxmlNode | string>;
+  }
+
+  export interface TxmlParseOptions {
+    keepComments?: boolean;
+    keepWhitespace?: boolean;
+    simplify?: boolean;
+  }
+
+  export function parse(
+    source: string,
+    options?: TxmlParseOptions,
+  ): Array<TxmlNode | string>;
+}
