2f26367a27d27569a6c8394440d2f59c39a5d23b
khuonghung <khuonghung1423@gmail.com>
2026-08-14 22:50:37 +0700

    feat: expose common parser helpers

---
+++ src/common/index.ts
@@ -0,0 +1,10 @@
+export { base64ArrayBuffer, encodeBase64 } from './binary/base64';
+export {
+  extractFileExtension,
+  getMimeType,
+  isVideoLink,
+} from './media/media-type';
+export { angleToDegrees, numberToFixed, toHex } from './numbers';
+export { escapeHtml, hasValidText } from './text/html';
+export { eachElement, getTextByPathList } from './xml/tree';
+export type { XmlLookupValue } from './xml/tree';
